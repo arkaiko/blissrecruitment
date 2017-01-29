8fe03a1581d9ba7ff5cbac1517bb45a8f638ea43
Sérgio Fernandes <sergio@lightspeck.com>
2017-01-29 12:41:36 +0000

    First commit

---
+++ src/app/look-for-server-health/look-for-server-health.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-look-for-server-health',
+  templateUrl: './look-for-server-health.component.html',
+  styleUrls: ['./look-for-server-health.component.css']
+})
+export class LookForServerHealthComponent {
+
+}
