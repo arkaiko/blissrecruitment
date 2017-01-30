00399a90d86c500b7f8cfb8c4b8a33e5ec4da131
Sérgio Fernandes <sergio@lightspeck.com>
2017-01-30 02:18:03 +0000

    Add "Back to List" and Voting Buttons to Details Screen.

--- src/app/details-screen/details-screen.component.ts
+++ src/app/details-screen/details-screen.component.ts
@@ -30,4 +30,45 @@ export class DetailsScreenComponent implements OnInit {
       .map(res => res.json())
       .subscribe(questions => this.question = questions);
   }
+  vote(voteIn) {
+    var body = {
+      'id': 1,
+      'image_url': 'https://dummyimage.com/600x400/000/fff.png&text=question+1+image+(600x400)',
+      'thumb_url': 'https://dummyimage.com/120x120/000/fff.png&text=question+1+image+(120x120)',
+      'question': 'Favourite programming language?',
+      'choices': [
+        {
+          'choice': 'Swift',
+          'votes': 0
+        },
+        {
+          'choice': 'Python',
+          'votes': 0
+        },
+        {
+          'choice': 'Objective-C',
+          'votes': 0
+        },
+        {
+          'choice': 'Ruby',
+          'votes': 0
+        }
+      ]
+    };
+
+    body.choices[voteIn].votes = 1;
+
+    var request = new XMLHttpRequest();
+
+    request.open('PUT', 'https://private-anon-99d310306c-blissrecruitmentapi.apiary-mock.com/questions/1');
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        console.log('Status:', this.status);
+        console.log('Headers:', this.getAllResponseHeaders());
+        console.log('Body:', this.responseText);
+      }
+    };
+    request.send(JSON.stringify(body));
+  }
 }
