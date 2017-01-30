f52f91daecf92ff845a5ecd34f9f5e6c76d4befb
Sérgio Fernandes <sergio@lightspeck.com>
2017-01-30 01:19:22 +0000

    Show data in detail screen

--- src/app/details-screen/details-screen.component.ts
+++ src/app/details-screen/details-screen.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
+import { URLSearchParams, } from '@angular/http';
 
 @Component({
   selector: 'app-details-screen',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsScreenComponent implements OnInit {
 
-  constructor() { }
+  question_id = 0;
+  question = {};
+
+  constructor(private route: ActivatedRoute, private http: Http) { }
 
   ngOnInit() {
+    this.route
+      .queryParams
+      .subscribe(params => {
+          this.question_id = params['question_id'];
+          this.fetchData(this.question_id);
+      });
   }
 
+  fetchData(question_id) {
+    this.http.get('https://private-anon-99d310306c-blissrecruitmentapi.apiary-mock.com/questions/'+question_id)
+      .map(res => res.json())
+      .subscribe(questions => this.question = questions);
+  }
 }
